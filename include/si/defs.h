0b01338ea1cdcd3d9eadfb760e43461e6b4cf06a
albertl <albertl@safeai.ai>
2024-05-16 17:41:44 -0700

    add c_cpp_properties.json and clean up

--- include/si/defs.h
+++ include/si/defs.h
@@ -1,4 +1,4 @@
-typedef bool bool8_t;  ///< A strongly sized SAF boolean type.
-typedef char char8_t;  ///< A strongly sized SAF character type.
-typedef float float32_t;  ///< A strongly sized SAF 32-bit float type.
-typedef double float64_t;  ///< A strongly sized SAF 64-bit float type.
+typedef bool bool8_t;  
+typedef char char8_t;  
+typedef float float32_t; 
+typedef double float64_t;  
